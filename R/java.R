b6e30f03d339dbe170861993536e006b8e43dd1b
Matt Pollock <mpollock@mitre.org>
2017-02-06 15:56:44 -0500

    fixed bug in how java was found on linux

--- R/java.R
+++ R/java.R
@@ -46,6 +46,8 @@ find_java <- function() {
     return(java_home)
   }
   
+  java_home <- NULL
+  
   # no option set, start looking
   if (is_windows()) {
     standard_path <- "C:/Program Files/Java/"
@@ -54,8 +56,10 @@ find_java <- function() {
   } else if (is_unix()) {
     java_dirs <- suppressWarnings(system("which java", intern=TRUE, ignore.stderr=TRUE))
     if (length(java_dirs)==0 || grepl("^which: no java", java_dirs)) {
-      standard_path <- "/usr/bin"
+      standard_path <- "/usr/lib"
       java_dirs <- dir(standard_path, full.names=TRUE)
+    } else {
+      java_home <- gsub("/bin/java$", "", java_dirs)
     }
     
   } else {
@@ -63,13 +67,16 @@ find_java <- function() {
     return("")
   }
   
-  java_dirs <- java_dirs[grepl("^jre|jdk|java", basename(java_dirs))]
-  if (length(java_dirs)==0) {
-    warning("Unable to find java. Use set_java_home with full path input.")
-    return("")
+  if (is.null(java_home)) {
+    java_dirs <- java_dirs[grepl("^jre|jdk", basename(java_dirs))]
+    if (length(java_dirs)==0) {
+      warning("Unable to find java. Use set_java_home with full path input.")
+      return("")
+    }
+    
+    java_home <- sort(java_dirs, decreasing=TRUE)[1]
   }
   
-  java_home <- sort(java_dirs, decreasing=TRUE)[1]
   set_var("java.home", java_home)
   return(java_home)
 }
