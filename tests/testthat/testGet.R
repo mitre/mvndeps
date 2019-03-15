37520fc288543127a83300a43fe046391434acb4
Matt Pollock <mpollock@mitre.org>
2019-03-15 13:12:16 -0400

    update tests to reflect full name of dependency plugin added in 48acd42e3ca9a17911f8e0852d05e338dac4da8f

--- tests/testthat/testGet.R
+++ tests/testthat/testGet.R
@@ -1,11 +1,11 @@
 context("dependency download commands")
 
 test_that("Non-transitive dependency download generates correct cmd option", {
-  cmd <- .download_dependency_cmd('bar','com.foo','1.0-SNAPSHOT',mvn='/usr/bin/mvn', transitive=FALSE)
-  expect_equal(cmd,'/usr/bin/mvn dependency:get -Dartifact=com.foo:bar:1.0-SNAPSHOT -Dtransitive=false')
+  cmd <- .download_dependency_cmd('bar', 'com.foo', '1.0-SNAPSHOT', mvn = '/usr/bin/mvn', transitive = FALSE, dep_plugin_version = "3.1.1")
+  expect_equal(cmd, '/usr/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -Dartifact=com.foo:bar:1.0-SNAPSHOT -Dtransitive=false')
 })
 
 test_that("Dependency with no transitive arg is transitive by default", {
-  cmd <- .download_dependency_cmd('bar','com.foo','1.0-SNAPSHOT',mvn='/usr/bin/mvn')
-  expect_equal(cmd,'/usr/bin/mvn dependency:get -Dartifact=com.foo:bar:1.0-SNAPSHOT -Dtransitive=true')
+  cmd <- .download_dependency_cmd('bar', 'com.foo', '1.0-SNAPSHOT', mvn = '/usr/bin/mvn', dep_plugin_version = "3.1.1")
+  expect_equal(cmd, '/usr/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -Dartifact=com.foo:bar:1.0-SNAPSHOT -Dtransitive=true')
 })
