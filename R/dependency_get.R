96eb921ec16bf5fef40171ca41a4fe977e876652
Matt Pollock <mpollock@mitre.org>
2020-02-13 20:58:01 -0500

    finish refactor

--- R/dependency_get.R
+++ R/dependency_get.R
@@ -4,7 +4,7 @@
 #' repository. It will not resolve the classpath in the sense of returning a list of jars to be added
 #' to the java classpath. For that use case see \code{\link{build_classpath}}.
 #'
-#' Under the hood this is using the maven dependency:get plugin. See
+#' Under the hood this is using the maven \code{dependency:get} plugin. See
 #' https://maven.apache.org/plugins/maven-dependency-plugin/get-mojo.html for details. One consequence
 #' of this is that each artifact in the input must be downloaded separately. However common transitive
 #' dependencies will not be redownloaded as maven will detect that a given transitive dependency is already
@@ -17,15 +17,20 @@
 #'   of the form 'id::[layout]::url', or simply using the URL is ok. For example, for maven central you may use
 #'   'central::default::http://repo1.maven.apache.org/maven2'. If missing the repositories configured in the user's
 #'   \code{settings.xml} will be used.
-#' @param dep_plugin_version Character. The version of the maven dependency plugin. See https://github.com/apache/maven-dependency-plugin/releases
-#'   for release versions.
+#' @param dep_plugin_version Character. The version of the maven dependency plugin. See
+#'   https://github.com/apache/maven-dependency-plugin/releases for release versions. If nothing is provided then
+#'   a default version is chosen (may change over time with releases of this package).
 #' @param verbose Logical. Dump maven output to console.
 #'
 #' @importFrom purrr %>% map map2 map_int pluck
 #' @return an integer vector of status codes, one for each artifact listed. Status code 0 indicates that the artifact
 #'   was successfully retrieved.
 #' @export
-get_dependencies <- function(artifacts, transitive = TRUE, remote_repos = NULL, dep_plugin_version = NULL, verbose = FALSE) {
+get_dependencies <- function(artifacts,
+                             transitive = TRUE,
+                             remote_repos = NULL,
+                             dep_plugin_version = NULL,
+                             verbose = FALSE) {
 
   if (is.null(dep_plugin_version))
     dep_plugin_version <- .globals$dep_plugin_version
@@ -50,8 +55,6 @@ get_dependencies <- function(artifacts, transitive = TRUE, remote_repos = NULL, 
 #'
 #' This function will build a system command using the maven dependency get funtionality to download a (java)
 #' dependency.
-#'
-#' @inheritParams download_dependency
 #'
 #' @keywords internal
 .dependency_get_args <- function(artifact, transitive = TRUE, remote_repos = NULL, dep_plugin_version) {
@@ -86,7 +89,7 @@ print_get_dependencies_messages <- function(artifact, sys_res, verbose) {
 #' if the dependency has not yet been downloaded (e.g., via \code{\link{download_dependency}})
 #' then the return value will be \code{NULL}.
 #'
-#' @inheritParams download_dependency
+#' @inheritParams get_dependencies
 #' @seealso \link{get_dependencies}
 #' @importFrom purrr %>% map_chr
 #' @export
@@ -100,7 +103,7 @@ find_artifacts <- function(artifacts, verbose = FALSE) {
 find_artifact <- function(artrifact, verbose) {
   # pull dependency apart. this is necessary to support use cases where the group and
   # version inputs are missing
-  parsed_dep <- parse_dependency(dep)
+  parsed_dep <- parse_dependency(artrifact)
   parsed_dep[c("groupid")] <- gsub("\\.", "/", parsed_dep[c("groupid")])
   path <- paste0(find_local_mvn_repo(), "/", paste0(parsed_dep, collapse="/"))
   if (!dir.exists(path)) {
@@ -109,134 +112,11 @@ find_artifact <- function(artrifact, verbose) {
     path <- NULL
   }
   # unconfigure_mvndeps(quiet=quiet)
-  return(path)
+  artifact_name <- paste0(parsed_dep["artifactid"], "-", parsed_dep["version"], ".jar")
+  return(file.path(path, artifact_name))
 }
 
-
-
-
-
-
-
-
-
-#' Have maven download a dependency
-#'
-#' This function will use the maven dependency get funtionality to download a (java)
-#' dependency.
-#'
-#' @param dep Character. The dependency name. This could be the entire name (e.g.,
-#'   group:dependency:version) in which case the other inputs may be omitted. Otherwise
-#'   it can be just the dependency name (i.e., the part in the middle of the above example).
-#' @param group Character. The maven artifact group name.
-#' @param version Character. The desired version of the dependency.
-#' @param mvn Character. The path the the maven installation.
-#' @param java_home Character. Path to java. If not provided the standard install paths
-#'   (platform dependent) will be checked.
-#' @param transitive Logical. If \code{TRUE}, download transitively, retrieving the specified artifact and all of its dependencies.
-#' @param quiet Logical. If \code{FALSE} status messages and some logs/warnings will be printed to the console.
-#' @param dep_plugin_version Character. The version of the maven dependency plugin. See https://github.com/apache/maven-dependency-plugin/releases
-#'   for release versions.
-#' @export
-download_dependency <- function(dep, group, version, mvn = find_mvn(), java_home, transitive = TRUE, quiet = FALSE,
-                                dep_plugin_version = "3.1.1") {
-  .Deprecated("get_dependencies")
-
-  artifact <- concatenate_dependency(dep, group, verion)
-  return(get_dependencies(artifacts = artifact, transitive = transitive, dep_plugin_version = dep_plugin_version))
-}
-
-#' Have maven find a dependency
-#'
-#' This function will find a maven dependency in your local maven repository. Note that
-#' if the dependency has not yet been downloaded (e.g., via \code{\link{download_dependency}})
-#' then the return value will be \code{NULL}.
-#'
-#' @inheritParams download_dependency
-#' @seealso \link{download_dependency}
-#' @export
-find_dependency_path <- function(dep, group, version, mvn=find_mvn(), java_home, quiet=FALSE) {
-  .Deprecated("find_artifact")
-
-  # configure_mvndeps(mvn=mvn, java_home=java_home, quiet=quiet)
-
-  # put dependency together
-  dep <- concatenate_dependency(dep, group, version)
-
-  # pull dependency apart. this is necessary to support use cases where the group and
-  # version inputs are missing
-  parsed_dep <- parse_dependency(dep)
-  parsed_dep[c("groupid")] <- gsub("\\.", "/", parsed_dep[c("groupid")])
-  path <- paste0(find_local_mvn_repo(mvn), "/", paste0(parsed_dep, collapse="/"))
-  if (!dir.exists(path)) {
-    if (!quiet)
-      warning(paste("Dependency not found in", path))
-    path <- NULL
-  }
-  # unconfigure_mvndeps(quiet=quiet)
-  return(path)
-}
-
-#' @rdname find_dependency_path
-#' @export
-find_dependency_jar <- function(dep, group, version, mvn = find_mvn(), java_home, quiet = FALSE) {
-  .Deprecated("find_artifact")
-
-  # put dependency together
-  dep <- concatenate_dependency(dep, group, version)
-
-  # find path
-  path <- find_dependency_path(dep=dep, mvn=mvn, java_home=java_home, quiet=quiet)
-  if (is.null(path))
-    return(NULL)
-
-  # get jar name by pealing off group
-  parsed_dep <- parse_dependency(dep)
-  jar_name <- paste0(parsed_dep["artifactid"], "-", parsed_dep["version"], ".jar")
-  jar_path <- file.path(path, jar_name)
-
-  # find jar
-  if (!file.exists(jar_path))
-    stop(paste0("The jar '", jar_name, "' was not found in the expected location '", path, "'."))
-
-  return(jar_path)
-}
-
-#' Have maven get a dependency
-#'
-#' This function wraps \code{\link{download_dependency}} and \code{\link{find_dependency_path}}.
-#' It will look to see if the dependency is already available in the local maven repository.
-#' If it is it will return the path. If it isn't then it will download the dependency and then
-#' return the path.
-#'
-#' @inheritParams download_dependency
-#' @export
-get_dependency_path <- function(dep, group, version, mvn = find_mvn(), java_home, quiet = FALSE) {
-  .Deprecated("find_artifact")
-
-  path <- find_dependency_path(dep=dep, group=group, version=version, mvn=mvn, java_home=java_home, quiet=TRUE)
-  if (!is.null(path))
-    return(path)
-
-  # dependency not found in local repo, try downloading it
-  download_dependency(dep=dep, group=group, version=version, mvn=mvn, java_home=java_home, quiet=quiet)
-  path <- find_dependency_path(dep=dep, group=group, version=version, mvn=mvn, java_home=java_home, quiet=quiet)
-  return(path)
-}
-
-#' @rdname get_dependency_path
-#' @export
-get_dependency_jar <- function(dep, group, version, mvn=find_mvn(), java_home, quiet=FALSE) {
-  .Deprecated("find_artifact")
-
-  # force download if needed
-  invisible(get_dependency_path(dep=dep, group=group, version=version, mvn=mvn, java_home=java_home, quiet=quiet))
-
-  # get jar
-  return(find_dependency_jar(dep=dep, group=group, version=version, mvn=mvn, java_home=java_home, quiet=quiet))
-}
-
-concatenate_dependency <- concatenate_dependencies <- function(dep, group, version) {
+concatenate_dependency <- function(dep, group, version) {
   if (!missing(group))
     dep <- paste0(group, ":", dep)
 
